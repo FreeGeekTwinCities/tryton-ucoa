7c60c79d43136da2c934b83a431b5e6e5fb13cda
Brian Dunnette <dunn0172@umn.edu>
2012-11-05 09:24:44 -0600

    initial (git) commit - tested with Tryton 2.0.x

---
+++ __tryton__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "Unified Chart of Accounts for US Non-Profits",
+    "version" : "2.0.0",
+    "author" : "Free Geek Twin Cities",
+    'email': 'brian@freegeektwincities.org',
+    'website': 'http://tryton-ucoa.googlecode.com/',
+    "category" : "Accounting",
+    "description": "Defines an account template for US non-profit/charitable organizations.",
+    "depends" : [
+        "account",
+    ],
+    "xml" : [
+        "account_ucoa.xml",
+    ],
+}
+
