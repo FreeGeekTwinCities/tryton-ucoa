7d3a03d904f26596f73c6e72987ee732e329c27d
Brian Dunnette <brian@dunnette.us>
2012-11-17 15:29:32 -0600

    version number bump

--- __tryton__.py
+++ __tryton__.py
@@ -1,9 +1,9 @@
 {
     "name" : "Unified Chart of Accounts for US Non-Profits",
-    "version" : "2.0.0",
+    "version" : "2.4.0",
     "author" : "Free Geek Twin Cities",
     'email': 'brian@freegeektwincities.org',
-    'website': 'http://tryton-ucoa.googlecode.com/',
+    'website': 'http://github.com/freegeektwincities/tryton-ucoa',
     "category" : "Accounting",
     "description": "Defines an account template for US non-profit/charitable organizations.",
     "depends" : [
